722da4bde506e4dabe2055f82434ad59eb03c6a7
Gyf_linux <guo812773673@163.com>
2018-04-26 16:17:47 +0800

    Site updated: 2018-04-26 16:17:46

--- js/src/schemes/pisces.js
+++ js/src/schemes/pisces.js
@@ -1,3 +1,3 @@
-// build time:Wed Apr 25 2018 01:51:16 GMT+0800 (CST)
+// build time:Thu Apr 26 2018 16:14:36 GMT+0800 (CST)
 $(document).ready(function(){var n=$(".header-inner");var r=$("#sidebar");var e=function(){return n.height()+CONFIG.sidebar.offset};var i=function(n){return r.css({"margin-top":n})};var a=window.matchMedia("(min-width: 991px)");i(e()).show();a.addListener(function(n){if(n.matches){i(e())}})});
 //rebuild by neat 
