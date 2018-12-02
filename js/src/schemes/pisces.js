bc519fc21c87ee3c551f33169480a4acf79ca2d0
afei <guo812773673@163.com>
2018-12-02 22:01:56 +0800

    Site updated: 2018-12-02 22:01:54

--- js/src/schemes/pisces.js
+++ js/src/schemes/pisces.js
@@ -1,3 +1,3 @@
-// build time:Sat Dec 01 2018 17:22:35 GMT+0800 (CST)
+// build time:Sun Dec 02 2018 22:01:46 GMT+0800 (CST)
 $(document).ready(function(){var n=$(".header-inner");var r=$("#sidebar");var e=function(){return n.height()+CONFIG.sidebar.offset};var i=function(n){return r.css({"margin-top":n})};var a=window.matchMedia("(min-width: 991px)");i(e()).show();a.addListener(function(n){if(n.matches){i(e())}})});
 //rebuild by neat 
